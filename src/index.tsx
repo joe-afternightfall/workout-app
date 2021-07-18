2ea013015ea58c9d65bd457a1d4d034c49b8de0b
joe-afternightfall <joe@afternightfall.com>
2021-07-17 20:46:59 -0500

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './configs/report-web-vitals';
 
 ReactDOM.render(
   <React.StrictMode>
