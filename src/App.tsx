da5df79bd637e818ac5275ddff351e37a0a456c3
joe-afternightfall <joe@afternightfall.com>
2021-07-17 22:07:59 -0500

    app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Theme,
+  WithStyles,
+  withStyles,
+  StyledComponentProps,
+  ThemeProvider as MuiThemeProvider,
+} from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { Styles } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { getLightTheme } from './configs/theme/light-theme';
+import PermSideDrawer from './components/app-shell/side-drawer/PermSideDrawer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles: Styles<Theme, StyledComponentProps> = (theme: Theme) => ({
+  root: {
+    display: 'flex',
+    // marginLeft: theme.spacing(2),
+    // marginRight: theme.spacing(2),
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+});
+
+class App extends Component<AppProps> {
+  render(): JSX.Element {
+    const { classes } = this.props;
+
+    return (
+      <MuiThemeProvider theme={getLightTheme()}>
+        <div className={classes.root}>
+          <CssBaseline />
+          <PermSideDrawer />
+
+          <main className={classes.content}>
+            <div>{this.props.children}</div>
+          </main>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-export default App;
+export interface AppProps extends WithStyles<typeof styles> {
+  children: JSX.Element;
+}
+
+export default withStyles(styles, { withTheme: true })(App);
