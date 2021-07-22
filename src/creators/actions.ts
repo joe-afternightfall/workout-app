40560753f988f4e63d4f2b08473bcbfe76b11d97
joe-afternightfall <joe@afternightfall.com>
2021-07-22 10:30:39 -0500

    action update

--- src/creators/actions.ts
+++ src/creators/actions.ts
@@ -1,10 +1,17 @@
 import { LoadExercisesAction, UpdateWarmUpsAction } from './exercise';
+import { LocationChangeAction } from 'connected-react-router';
+import { LoadMuscleGroupsAction } from './muscle-groups';
 
 export enum ActionTypes {
   // Application Actions
   INITIALIZE = 'INITIALIZE',
   UPDATE_WARM_UPS = 'UPDATE_WARM_UPS',
   LOAD_EXERCISES = 'LOAD_EXERCISES',
+  LOAD_MUSCLE_GROUPS = 'LOAD_MUSCLE_GROUPS',
 }
 
-export type ApplicationActions = UpdateWarmUpsAction | LoadExercisesAction;
+export type ApplicationActions =
+  | LocationChangeAction
+  | UpdateWarmUpsAction
+  | LoadExercisesAction
+  | LoadMuscleGroupsAction;
