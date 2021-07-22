0f8aff0f26950facc405a3305c40852440d8cd6e
joe-afternightfall <joe@afternightfall.com>
2021-07-22 10:29:55 -0500

    updating initializer

--- src/firebase/Initializer.ts
+++ src/firebase/Initializer.ts
@@ -1,6 +1,7 @@
 import { Store } from 'redux';
 import firebase from 'firebase';
 import { updateExercises } from './update-methods/update-exercises';
+import { updateMuscleGroups } from './update-methods/update-muscle-groups';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -26,6 +27,7 @@ export class Initializer {
     firebase.analytics();
 
     const exercises = firebase.database().ref('/exercises');
+    const muscleGroups = firebase.database().ref('/muscle-groups');
 
     exercises.on('child_added', async () => {
       await updateExercises(this.store);
@@ -37,6 +39,18 @@ export class Initializer {
 
     exercises.on('child_removed', async () => {
       await updateExercises(this.store);
+    });
+
+    muscleGroups.on('child_added', async () => {
+      await updateMuscleGroups(this.store);
+    });
+
+    muscleGroups.on('child_changed', async () => {
+      await updateMuscleGroups(this.store);
+    });
+
+    muscleGroups.on('child_removed', async () => {
+      await updateMuscleGroups(this.store);
     });
   }
 }
