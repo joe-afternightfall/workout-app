8495f62e421400028554cb039042a8c34ad61f4d
joe-afternightfall <joe@afternightfall.com>
2021-07-22 10:31:09 -0500

    adding get and delete muscle groups

--- src/services/muscle-group-service.ts
+++ src/services/muscle-group-service.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import { MuscleGroupDAO } from '../configs/models/MuscleGroupDAO';
 import { v4 as uuidv4 } from 'uuid';
+import { ExerciseDAO } from '../configs/models/ExerciseDAO';
 
 export const createNewMuscleGroup = async (name: string): Promise<void> => {
   const ref = firebase.database().ref(`/muscle-groups`);
@@ -15,4 +16,28 @@ export const createNewMuscleGroup = async (name: string): Promise<void> => {
       return Promise.resolve();
     }
   });
+};
+
+export const getAllMuscleGroups = async (): Promise<ExerciseDAO> => {
+  return await firebase
+    .database()
+    .ref('/muscle-groups')
+    .once('value')
+    .then((snapshot) => {
+      return snapshot.val();
+    });
+};
+
+export const deleteMuscleGroup = async (id: string): Promise<void> => {
+  return await firebase
+    .database()
+    .ref('/muscle-groups')
+    .child(id)
+    .remove((error) => {
+      if (error) {
+        return Promise.reject();
+      } else {
+        return Promise.resolve();
+      }
+    });
 };
