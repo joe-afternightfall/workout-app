aec25ecfcf9554ef81af528311e52e1655e68a4a
joe-afternightfall <joe@afternightfall.com>
2021-07-21 22:04:03 -0500

    updating actions

--- src/creators/actions.ts
+++ src/creators/actions.ts
@@ -1,4 +1,10 @@
-export default {
+import { LoadExercisesAction, UpdateWarmUpsAction } from './exercise';
+
+export enum ActionTypes {
   // Application Actions
-  INITIALIZE: 'INITIALIZE',
-};
+  INITIALIZE = 'INITIALIZE',
+  UPDATE_WARM_UPS = 'UPDATE_WARM_UPS',
+  LOAD_EXERCISES = 'LOAD_EXERCISES',
+}
+
+export type ApplicationActions = UpdateWarmUpsAction | LoadExercisesAction;
