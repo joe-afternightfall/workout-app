a13347393d5ae69c19aa625715df71e58eb7cb16
joe-afternightfall <joe@afternightfall.com>
2021-07-21 22:24:25 -0500

    updating creator

--- src/creators/exercise.ts
+++ src/creators/exercise.ts
@@ -1,18 +1,17 @@
-import { Exercise } from '../configs/app-types';
 import { ActionTypes } from './actions';
 import { ExerciseVO } from '../configs/models/ExerciseVO';
 
-export interface UpdateWarmUpsAction {
-  type: ActionTypes.UPDATE_WARM_UPS;
-  exercise: Exercise;
-}
-
-export const updateWarmUps = (props: Exercise): UpdateWarmUpsAction => {
-  return {
-    type: ActionTypes.UPDATE_WARM_UPS,
-    exercise: props,
-  };
-};
+// export interface UpdateWarmUpsAction {
+//   type: ActionTypes.UPDATE_WARM_UPS;
+//   exercise: Exercise;
+// }
+//
+// export const updateWarmUps = (props: Exercise): UpdateWarmUpsAction => {
+//   return {
+//     type: ActionTypes.UPDATE_WARM_UPS,
+//     exercise: props,
+//   };
+// };
 
 export interface LoadExercisesAction {
   type: ActionTypes.LOAD_EXERCISES;
