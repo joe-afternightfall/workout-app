ba02c254cb57e4756bde34d5a304dae8edb94428
joe-afternightfall <joe@afternightfall.com>
2021-07-22 23:51:22 -0500

    service setup

--- src/services/exercise-service.ts
+++ src/services/exercise-service.ts
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
+import { v4 as uuidv4 } from 'uuid';
 import { ExerciseDAO } from '../configs/models/ExerciseDAO';
 
-export const createNewExercise = async (
-  exercise: ExerciseDAO
-): Promise<void> => {
+export const createNewExercise = async (name: string): Promise<void> => {
   const ref = firebase.database().ref(`/exercises`);
   const newRef = ref.push();
 
-  return await newRef.set(exercise, (error: Error | null) => {
+  const exerciseDAO = new ExerciseDAO(uuidv4(), name);
+
+  return await newRef.set(exerciseDAO, (error: Error | null) => {
     if (error) {
       return Promise.reject();
     } else {
@@ -23,5 +24,19 @@ export const getAllExercises = async (): Promise<ExerciseDAO> => {
     .once('value')
     .then((snapshot) => {
       return snapshot.val();
+    });
+};
+
+export const deleteExercise = async (id: string): Promise<void> => {
+  return await firebase
+    .database()
+    .ref('/exercises')
+    .child(id)
+    .remove((error) => {
+      if (error) {
+        return Promise.reject();
+      } else {
+        return Promise.resolve();
+      }
     });
 };
