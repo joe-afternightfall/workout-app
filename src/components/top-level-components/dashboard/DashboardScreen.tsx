fb373cf4d4994d8e062a6ea126e8b2179cafd617
joe-afternightfall <joe@afternightfall.com>
2021-07-22 22:27:43 -0500

    moving

--- src/components/top-level-components/dashboard/DashboardScreen.tsx
+++ src/components/top-level-components/dashboard/DashboardScreen.tsx
@@ -5,19 +5,17 @@ import {
   StyledComponentProps,
 } from '@material-ui/core/styles';
 import React, { Component } from 'react';
-import { Styles } from '@material-ui/styles';
-import MuscleGroupsCard from './dashboard/MuscleGroupsCard';
 import { Grid } from '@material-ui/core';
-import ExerciseCard from './dashboard/ExerciseCard';
+import { Styles } from '@material-ui/styles';
+import ExerciseCard from './exercises/ExerciseCard';
+import MuscleGroupsCard from './muscle-groups/MuscleGroupsCard';
 
 const styles: Styles<Theme, StyledComponentProps> = () => ({});
 
 class DashboardScreen extends Component<DashboardScreenProps> {
   render(): JSX.Element {
-    const { classes } = this.props;
-
     return (
-      <Grid xs={12} item container>
+      <Grid xs={12} item container spacing={2}>
         <Grid item xs={12}>
           <span>{'Dashboard'}</span>
         </Grid>
