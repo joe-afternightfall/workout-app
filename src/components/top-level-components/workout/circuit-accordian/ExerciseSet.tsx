110a1f70a9d8b030ab9fd186fbdd20fcb811355b
joe-afternightfall <joe@afternightfall.com>
2021-07-24 10:44:04 -0400

    adding exercise set

---
+++ src/components/top-level-components/workout/circuit-accordian/ExerciseSet.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Button, Grid, TextField, Typography } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/CheckCircleOutlined';
+import RemoveIcon from '@material-ui/icons/RemoveCircleOutlineOutlined';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    confirmButton: {
+      background: '#C9F0DE',
+      color: '#008E62',
+      '&:hover': {
+        background: '#A2EAC9',
+      },
+    },
+    completedRow: {
+      background: '#E4F3EC',
+    },
+    deleteButton: {
+      background: '#F5DADB',
+      color: '#D41417',
+      '&:hover': {
+        background: '#F4C3C3',
+      },
+    },
+  })
+);
+
+export default function ExerciseSet(props: RepRowProps): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={12}>
+      <Grid item xs={12} container alignItems={'center'} spacing={2}>
+        <Grid item xs={2}>
+          <Typography>{'1'}</Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          <TextField style={{ width: '100%' }} variant={'outlined'} />
+        </Grid>
+
+        <Grid item xs={3}>
+          <TextField style={{ width: '100%' }} variant={'outlined'} />
+        </Grid>
+
+        <Grid item xs={4}>
+          <Grid container alignItems={'center'} justify={'center'} spacing={2}>
+            <Grid item>
+              <Button className={classes.deleteButton}>
+                <RemoveIcon />
+              </Button>
+            </Grid>
+
+            <Grid item>
+              <Button className={classes.confirmButton}>
+                <CheckIcon />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export interface RepRowProps {
+  setNumber: number;
+
+}
