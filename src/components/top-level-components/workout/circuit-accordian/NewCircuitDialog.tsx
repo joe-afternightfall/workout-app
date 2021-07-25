fe9aa5e515a83658b1b718a1de041f207f5f4522
joe-afternightfall <joe@afternightfall.com>
2021-07-25 13:21:10 -0400

    creating circuit dialog

---
+++ src/components/top-level-components/workout/circuit-accordian/NewCircuitDialog.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ListItem from '@material-ui/core/ListItem';
+import { Button, Dialog, Grid, List } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import BaseDialogContent from '../../../app-shell/BaseDialogContent';
+import { NewCircuitProps } from '../WorkoutScreen';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {},
+  })
+);
+
+export default function NewCircuitDialog(
+  props: NewCircuitDialogProps
+): JSX.Element {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClick = (type: string) => {
+    props.addCircuitHandler({
+      id: uuidv4(),
+      type: type,
+    });
+  };
+
+  return (
+    <>
+      <Button variant={'outlined'} color={'primary'} onClick={handleClickOpen}>
+        {'Click to add circuit'}
+      </Button>
+
+      <Dialog onClose={handleClose} open={open} maxWidth={'xs'}>
+        <BaseDialogContent
+          title={'New Circuit'}
+          closeClickHandler={handleClose}
+          dialogContent={
+            <List>
+              <ListItem>
+                <Button
+                  fullWidth
+                  color={'primary'}
+                  variant={'contained'}
+                  onClick={() => {
+                    handleClick('Warm Ups');
+                  }}
+                >
+                  {'Warm Ups'}
+                </Button>
+              </ListItem>
+              <ListItem>
+                <Button
+                  fullWidth
+                  color={'primary'}
+                  variant={'contained'}
+                  onClick={() => {
+                    handleClick('Main Workout');
+                  }}
+                >
+                  {'Main Workout'}
+                </Button>
+              </ListItem>
+              <ListItem>
+                <Button
+                  fullWidth
+                  color={'primary'}
+                  variant={'contained'}
+                  onClick={() => {
+                    handleClick('Cool Down');
+                  }}
+                >
+                  {'Cool Down'}
+                </Button>
+              </ListItem>
+            </List>
+          }
+          dialogActions={<Button onClick={handleClose}>{'Cancel'}</Button>}
+        />
+      </Dialog>
+    </>
+  );
+}
+
+export interface NewCircuitDialogProps {
+  addCircuitHandler: (props: NewCircuitProps) => void;
+}
