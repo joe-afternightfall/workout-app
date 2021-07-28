074a7f2ca0951ab6cbaa04b446c4eb73169fe8f6
joe-afternightfall <joe@afternightfall.com>
2021-07-28 01:25:24 -0500

    adding circuit creator

--- src/creators/workout.ts
+++ src/creators/workout.ts
@@ -1,4 +1,5 @@
 import { ActionTypes } from './actions';
+import { NewCircuitProps } from '../components/top-level-components/workout/WorkoutScreen';
 
 export interface UpdateWorkoutDateAction {
   type: ActionTypes.UPDATE_WORKOUT_DATE;
@@ -9,5 +10,29 @@ export const updateWorkoutDate = (date: Date): UpdateWorkoutDateAction => {
   return {
     type: ActionTypes.UPDATE_WORKOUT_DATE,
     date: date,
+  };
+};
+
+export interface AddCircuitAction {
+  type: ActionTypes.ADD_CIRCUIT;
+  circuit: NewCircuitProps;
+}
+
+export const addCircuit = (circuit: NewCircuitProps): AddCircuitAction => {
+  return {
+    type: ActionTypes.ADD_CIRCUIT,
+    circuit: circuit,
+  };
+};
+
+export interface DeleteCircuitAction {
+  type: ActionTypes.DELETE_CIRCUIT;
+  id: string;
+}
+
+export const deleteCircuit = (id: string): DeleteCircuitAction => {
+  return {
+    type: ActionTypes.DELETE_CIRCUIT,
+    id: id,
   };
 };
