42978ff28392e5934a0b322edeec6926d061d441
joe-afternightfall <joe@afternightfall.com>
2021-08-06 11:57:27 -0500

    got update working for category table

--- src/components/top-level-components/exercises-screen/categories-table/CategoryTable.tsx
+++ src/components/top-level-components/exercises-screen/categories-table/CategoryTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
@@ -7,6 +7,9 @@ import PageTitle from '../PageTitle';
 import { WorkoutCategoryVO } from '../../../../configs/models/WorkoutCategoryVO';
 import { State } from '../../../../configs/redux/store';
 import { ExerciseVO } from '../../../../configs/models/ExerciseVO';
+import NewCategoryDialog from './NewCategoryDialog';
+import { TextField } from '@material-ui/core';
+import { updateWorkoutCategory } from '../../../../services/workout-categories-service';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,8 +17,32 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const editField = (props: {
+  value: string;
+  onChange: (value: string) => void;
+}) => {
+  return (
+    <TextField
+      value={props.value}
+      data-testid={'edit-url-text-field'}
+      onChange={(e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) =>
+        props.onChange(e.target.value)
+      }
+    />
+  );
+};
+
 const CategoryTable = (props: CategoryTableProps): JSX.Element => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const openDialog = () => {
+    setOpen(true);
+  };
+
+  const closeDialog = () => {
+    setOpen(false);
+  };
 
   const data = props.workoutCategories.map(
     (category: WorkoutCategoryVO, index: number) => {
@@ -34,6 +61,8 @@ const CategoryTable = (props: CategoryTableProps): JSX.Element => {
 
   return (
     <>
+      <NewCategoryDialog open={open} closeClickHandler={closeDialog} />
+
       <MaterialTable
         data={data}
         data-testid={'category-table'}
@@ -44,30 +73,27 @@ const CategoryTable = (props: CategoryTableProps): JSX.Element => {
           pageSizeOptions: [6, 12, 18],
           actionsColumnIndex: -1,
         }}
-        // editable={{
-        //   onRowUpdate: (rowData): Promise<void> =>
-        //     new Promise((resolve) => {
-        //       if (rowData.categoryId) {
-        //         updateExercise(
-        //           rowData.firebaseId,
-        //           rowData.exercise,
-        //           rowData.categoryId
-        //         ).then(() => {
-        //           setTimeout(() => {
-        //             resolve();
-        //           }, 1500);
-        //         });
-        //       }
-        //     }),
-        //   onRowDelete: (rowData): Promise<void> =>
-        //     new Promise((resolve) => {
-        //       deleteExercise(rowData.firebaseId).then(() => {
-        //         setTimeout(() => {
-        //           resolve();
-        //         }, 1500);
-        //       });
-        //     }),
-        // }}
+        editable={{
+          onRowUpdate: (rowData): Promise<void> =>
+            new Promise((resolve) => {
+              updateWorkoutCategory(
+                rowData.category.firebaseId,
+                rowData.category.name
+              ).then(() => {
+                setTimeout(() => {
+                  resolve();
+                }, 1500);
+              });
+            }),
+          onRowDelete: (rowData): Promise<void> =>
+            new Promise((resolve) => {
+              // deleteExercise(rowData.firebaseId).then(() => {
+              //   setTimeout(() => {
+              //     resolve();
+              //   }, 1500);
+              // });
+            }),
+        }}
         columns={[
           {
             title: '#',
@@ -83,11 +109,12 @@ const CategoryTable = (props: CategoryTableProps): JSX.Element => {
             cellStyle: {
               width: '40%',
             },
-            // editComponent: editField,
+            editComponent: editField,
           },
           {
             title: 'Workouts',
             field: 'exercises',
+            editable: 'never',
             cellStyle: {
               width: '30%',
             },
@@ -99,7 +126,7 @@ const CategoryTable = (props: CategoryTableProps): JSX.Element => {
             tooltip: 'Add New Category',
             isFreeAction: true,
             onClick: () => {
-              alert('hi');
+              openDialog();
             },
           },
         ]}
