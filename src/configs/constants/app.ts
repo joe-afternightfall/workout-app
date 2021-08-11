12e85696313961585391f9f97b72547f00e12bb7
joe-afternightfall <joe@afternightfall.com>
2021-08-11 09:18:52 -0500

    creating consts

--- src/configs/constants/app.ts
+++ src/configs/constants/app.ts
@@ -1,1 +1,7 @@
 export const FULL_DRAWER_WIDTH = '240PX';
+export const DASHBOARD_SCREEN_PATH = '/dashboard';
+export const WORKOUT_SCREEN_PATH = '/workout';
+export const USER_PROFILE_SCREEN_PATH = '/user-profile';
+export const EXERCISE_TYPES_SCREEN_PATH = '/configs/exercise-types';
+export const CATEGORY_TYPES_SCREEN_PATH = '/configs/category-types';
+export const CIRCUIT_TYPES_SCREEN_PATH = '/configs/circuit-types';
