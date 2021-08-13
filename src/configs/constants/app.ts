3d500af5494a8e2205efd94a6fdfd3c4a62644e7
joe-afternightfall <joe@afternightfall.com>
2021-08-13 16:23:48 -0500

    ripping out reg ex

--- src/configs/constants/app.ts
+++ src/configs/constants/app.ts
@@ -7,3 +7,5 @@ export const USER_PROFILE_SCREEN_PATH = '/user-profile';
 export const EXERCISE_TYPES_SCREEN_PATH = '/configs/exercise-types';
 export const CATEGORY_TYPES_SCREEN_PATH = '/configs/category-types';
 export const CIRCUIT_TYPES_SCREEN_PATH = '/configs/circuit-types';
+
+export const NUMBERS_ONLY_REGEX = new RegExp('^[0-9]*$');
