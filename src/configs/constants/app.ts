bfb09b9e487e9d145b000c20aab901ee32e50260
joe-afternightfall <joe@afternightfall.com>
2021-08-13 22:54:52 -0500

    allowing empty

--- src/configs/constants/app.ts
+++ src/configs/constants/app.ts
@@ -9,4 +9,4 @@ export const CATEGORY_TYPES_SCREEN_PATH = '/configs/category-types';
 export const CIRCUIT_TYPES_SCREEN_PATH = '/configs/circuit-types';
 
 export const NUMBERS_ONLY_REGEX = new RegExp('^[0-9]*$');
-export const DISPLAY_NAME_REGEX = new RegExp(/[A-Za-z0-9\-\\_]$/);
+export const DISPLAY_NAME_REGEX = new RegExp(/^$|[A-Za-z0-9\-\\_]$/);
