00e2cd8880edd4baa76ff4b82527f18a0cdd7e83
joe-afternightfall <joe@afternightfall.com>
2021-08-14 09:29:03 -0500

    creating functional component

---
+++ src/components/top-level-components/dashboard-screen/calendar/CalendarCard.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { Card } from '@material-ui/core';
+import CalendarCells from './components/CalendarCells';
+import { addMonths, format, subMonths } from 'date-fns';
+import CalendarHeader from './components/CalendarHeader';
+import CalendarControls from './components/CalendarControls';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { State } from '../../../../configs/redux/store';
+import { WorkoutVO } from '../../../../configs/models/WorkoutVO';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+  })
+);
+
+const CalendarCard = (props: CalendarCardProps): JSX.Element => {
+  const classes = useStyles();
+  const dateFormat = 'MMMM yyyy';
+  const today = new Date();
+  const [currentMonth, setCurrentMonth] = React.useState(today);
+  const [selectedDate, setSelectedDate] = React.useState(today);
+
+  const onDateClick = (day: Date): void => {
+    setSelectedDate(day);
+  };
+
+  const nextMonth = (): void => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
+
+  const prevMonth = (): void => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+
+  return (
+    <Card>
+      <CalendarControls
+        currentMonth={format(currentMonth, dateFormat)}
+        prevMonthClickHandler={prevMonth}
+        nextMonthClickHandler={nextMonth}
+      />
+      <CalendarHeader currentMonth={currentMonth} />
+      <CalendarCells
+        selectedDate={selectedDate}
+        dateClickHandler={onDateClick}
+        currentMonth={currentMonth}
+      />
+    </Card>
+  );
+};
+
+export interface CalendarCardProps {
+  userWorkouts: WorkoutVO[];
+}
+
+const mapStateToProps = (state: State): CalendarCardProps => {
+  return {
+    userWorkouts: state.applicationState.userWorkouts,
+  } as unknown as CalendarCardProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): CalendarCardProps =>
+  ({} as unknown as CalendarCardProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CalendarCard);
