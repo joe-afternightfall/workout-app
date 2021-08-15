cea372ce24e52f9f3b7dbf7496d5c6e73da7f862
joe-afternightfall <joe@afternightfall.com>
2021-08-15 12:44:28 -0500

    deleting

--- src/firebase/update-methods/category-types.ts
+++
@@ -1,9 +0,0 @@
-import { Store } from 'redux';
-import { loadCategoryTypes } from '../../creators/workout-configurations';
-import { getAllCategoryTypes } from '../../services/workout-configurations/category-types-service';
-
-export const updateCategoryTypes = async (store: Store): Promise<void> => {
-  const categoryTypes = await getAllCategoryTypes();
-
-  store.dispatch(loadCategoryTypes(categoryTypes));
-};
