8723bff872a7cebe2a63fcad0de1f51973712deb
joe-afternightfall <joe@afternightfall.com>
2021-08-15 22:48:19 -0500

    updating todo's

--- src/index.tsx
+++ src/index.tsx
@@ -32,30 +32,21 @@ reportWebVitals();
 
 // todo: dispatch action if user enters in bad credentials and show error text
 // todo: add workout timer to redux store.  on stop/pause/stop
-// todo: ******* close side bar on small break point
 // todo: create <AppDialog /> component
 // todo: ******* create <AppSnackbar /> component
 // todo: ******* allow for workout to be edited
-// todo: place weekly calendar/daily check on dashboard
 // todo: ******* add a daily counter for days in a row working out
-// todo: ******* add dots on calendar for days worked out
 // todo: add loader cards
-// todo: use react-calendar for custom calendar to view when we have worked out
-// todo: dots for dojo workouts
 // todo: dots for solo/other workouts
 // todo: allow for building "workout plan"
 // todo: add application snackbar and use when updating/modifying table info
-// todo: add full screen button
 // todo: allow setup for "SetType"
 // todo: create profile page to select icon and fill out info
-// todo: use profile page to determine if user has logged in before
-// todo: generate user id and start to save workouts under that ID
-// todo: use Brute icon for log in page
 // todo: cool down dialog in between workouts
 // todo: create "circuit builder" page
 // todo: create "workout" reducer
 // todo: make reducer better/extract out util methods
 // todo: allow user to add new category while adding exercise
-// todo: create user profile object
-// todo: start saving workouts under user profile
 // todo: display loader while waiting for user profile to not show dashboard
+// todo: don't allow work out date after today to be selected
+// todo: debug why new exercise dialog is broken for iPad
