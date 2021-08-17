d2e69de84eb9e83df5a0becf2b93a9ae25ca7080
joe-afternightfall <joe@afternightfall.com>
2021-08-17 09:46:25 -0500

    fixing add exercise dialog

--- src/components/top-level-components/workout-screen/circuit-accordian/dialogs/add-exercise/AddExerciseDialog.tsx
+++ src/components/top-level-components/workout-screen/circuit-accordian/dialogs/add-exercise/AddExerciseDialog.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ExerciseTabs from './ExerciseTabs';
-import { Button, Dialog } from '@material-ui/core';
+import CategoryList from './CategoryList';
+import ExerciseSwipeableView from './ExerciseSwipeableView';
+import { Button, Dialog, DialogContent, Grid } from '@material-ui/core';
 
 export default function AddExerciseDialog(
   props: AddExerciseDialogProps
 ): JSX.Element {
   const [open, setOpen] = React.useState(false);
+  const [activeTab, setActiveTab] = React.useState(0);
 
   const handleOpen = () => {
     setOpen(true);
@@ -15,15 +17,38 @@ export default function AddExerciseDialog(
     setOpen(false);
   };
 
+  const handleViewChange = (index: number) => {
+    setActiveTab(index);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setActiveTab(index);
+  };
+
   return (
     <>
       <Button onClick={handleOpen}>{'Add Exercise'}</Button>
 
-      <Dialog onClose={handleClose} open={open} fullWidth maxWidth={'md'}>
-        <ExerciseTabs
-          workoutCircuitId={props.circuitId}
-          closeClickHandler={handleClose}
-        />
+      <Dialog onClose={handleClose} open={open} fullWidth maxWidth={'lg'}>
+        <DialogContent style={{ padding: '24px 16px 24px 0' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <CategoryList
+                activeTab={activeTab}
+                clickHandler={handleViewChange}
+              />
+            </Grid>
+
+            <Grid item xs={9}>
+              <ExerciseSwipeableView
+                workoutCircuitId={props.circuitId}
+                closeClickHandler={handleClose}
+                selectedIndex={activeTab}
+                viewChangeHandler={handleChangeIndex}
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
       </Dialog>
     </>
   );
---
+++ src/components/top-level-components/workout-screen/circuit-accordian/dialogs/add-exercise/ExerciseSwipeableView.tsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import { Dispatch } from 'redux';
+import TabPanel from './TabPanel';
+import { connect } from 'react-redux';
+import SwipeableViews from 'react-swipeable-views';
+import { State } from '../../../../../../configs/redux/store';
+import { addExerciseToCircuit } from '../../../../../../creators/workout';
+import { List, ListItem, Button, Typography, Grid } from '@material-ui/core';
+import { useTheme, makeStyles, createStyles } from '@material-ui/core/styles';
+import { ExerciseTypeVO } from '../../../../../../configs/models/workout-configurations/exercise-type/ExerciseTypeVO';
+import { CategoryTypeVO } from '../../../../../../configs/models/workout-configurations/category-type/CategoryTypeVO';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      height: '100%',
+      borderRadius: 8,
+      backgroundColor: '#F6F6F6',
+    },
+  })
+);
+
+const ExerciseSwipeableView = (
+  props: ExerciseSwipeableViewProps & PassedInExerciseSwipeableViewProps
+): JSX.Element => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <SwipeableViews
+      axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+      index={props.selectedIndex}
+      onChangeIndex={props.viewChangeHandler}
+      className={classes.root}
+    >
+      {props.categoryTypes.map((category: CategoryTypeVO, index: number) => {
+        const categoryTypeId = props.categoryTypes[index].id;
+
+        const foundExercises = props.exerciseTypes.filter(
+          (exercise: ExerciseTypeVO) =>
+            exercise.workoutCategoryId === categoryTypeId
+        );
+
+        return (
+          <TabPanel
+            key={index}
+            value={props.selectedIndex}
+            index={index}
+            dir={theme.direction}
+          >
+            {foundExercises.length === 0 ? (
+              <Grid container justify={'center'}>
+                <Grid item>
+                  <Typography>
+                    {'no exercises setup for this category'}
+                  </Typography>
+                </Grid>
+              </Grid>
+            ) : (
+              <List>
+                {foundExercises.map(
+                  (exercise: ExerciseTypeVO, exerciseIndex: number) => {
+                    return (
+                      <ListItem key={exerciseIndex}>
+                        <Button
+                          fullWidth
+                          color={'primary'}
+                          variant={'contained'}
+                          onClick={() => {
+                            props.addExerciseHandler(
+                              props.workoutCircuitId,
+                              exercise.id
+                            );
+                            props.closeClickHandler();
+                          }}
+                        >
+                          {exercise.name}
+                        </Button>
+                      </ListItem>
+                    );
+                  }
+                )}
+              </List>
+            )}
+          </TabPanel>
+        );
+      })}
+    </SwipeableViews>
+  );
+};
+
+export interface ExerciseSwipeableViewProps {
+  categoryTypes: CategoryTypeVO[];
+  exerciseTypes: ExerciseTypeVO[];
+  addExerciseHandler: (circuitId: string, exerciseId: string) => void;
+}
+
+export interface PassedInExerciseSwipeableViewProps {
+  closeClickHandler: () => void;
+  workoutCircuitId: string;
+  selectedIndex: number;
+  viewChangeHandler: (index: number) => void;
+}
+
+const mapStateToProps = (state: State): ExerciseSwipeableViewProps => {
+  return {
+    categoryTypes: state.applicationState.workoutConfigurations.categoryTypes,
+    exerciseTypes: state.applicationState.workoutConfigurations.exerciseTypes,
+  } as unknown as ExerciseSwipeableViewProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): ExerciseSwipeableViewProps =>
+  ({
+    addExerciseHandler: (circuitId: string, exerciseId: string) => {
+      dispatch(addExerciseToCircuit(circuitId, exerciseId));
+    },
+  } as unknown as ExerciseSwipeableViewProps);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ExerciseSwipeableView);
