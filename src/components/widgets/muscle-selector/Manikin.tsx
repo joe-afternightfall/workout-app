fb138c15c2e1dbdc702b5ebe61a7af07faade90e
joe-afternightfall <joe@afternightfall.com>
2021-08-18 14:26:00 -0500

    adding js to file

--- src/components/widgets/muscle-selector/Manikin.tsx
+++ src/components/widgets/muscle-selector/Manikin.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
 
+document
+  .querySelectorAll('.muscle-groups svg g g[id]')
+  .forEach(function (group) {
+    // For the hover
+    group.addEventListener('mouseover', function (el: any) {
+      let id = el.path[1].id.toLowerCase();
+      if (!id) id = el.path[2].id.toLowerCase();
+      const label = document.querySelectorAll('label[for=' + id + ']')[0];
+      if (label.classList) label.classList.add('hover');
+      else label.className += ' ' + 'hover';
+    });
+    group.addEventListener('mouseout', function (el: any) {
+      let id = el.path[1].id.toLowerCase();
+      if (!id) id = el.path[2].id.toLowerCase();
+      const label = document.querySelectorAll('label[for=' + id + ']')[0];
+      const clss = 'hover';
+      if (label.classList) label.classList.remove(clss);
+      else
+        label.className = label.className.replace(
+          new RegExp('(^|\\b)' + clss.split(' ').join('|') + '(\\b|$)', 'gi'),
+          ' '
+        );
+    });
+    // For the click
+    group.addEventListener('click', function (el: any) {
+      alert('clicked');
+      let id = el.path[1].id.toLowerCase();
+      if (!id) id = el.path[2].id.toLowerCase();
+      const input = document.getElementById(id) as HTMLInputElement;
+      if (input.checked) input.checked = false;
+      else input.checked = true;
+    });
+  });
+
 export default function Manikin(): JSX.Element {
   return (
     <svg
