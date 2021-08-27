61a09a54e0dcaf089e0a6be03b61e3f08ed7ef31
joe-afternightfall <joe@afternightfall.com>
2021-08-26 22:12:29 -0500

    mobile app router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,29 +3,53 @@ import React from 'react';
 import { Route } from 'react-router';
 import { connect } from 'react-redux';
 import { State } from './configs/redux/store';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { PageProps, routes } from './configs/constants/routes';
+import { useTheme } from '@material-ui/core/styles';
 import SignInScreen from './components/top-level-components/sign-in-screen/SignInScreen';
+import MobileApp from './MobileApp';
+import { mobileRoutes } from './configs/constants/mobile-routes';
 
 const AppRouter = (props: AppRouterProps): JSX.Element => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+
   return props.isValidated ? (
-    <App>
-      <div className={'route'}>
-        {Object.keys(routes).map((value: string) => {
-          return routes[value].pageProps.map(
-            (page: PageProps, index: number) => {
-              return (
-                <Route
-                  key={index}
-                  exact
-                  path={page.path}
-                  component={page.routerComponent}
-                />
-              );
-            }
-          );
-        })}
-      </div>
-    </App>
+    isMobile ? (
+      <MobileApp>
+        <div className={'route'}>
+          {Object.keys(mobileRoutes).map((value: string, index: number) => {
+            return (
+              <Route
+                key={index}
+                exact
+                path={mobileRoutes[value].path}
+                component={mobileRoutes[value].routerComponent}
+              />
+            );
+          })}
+        </div>
+      </MobileApp>
+    ) : (
+      <App>
+        <div className={'route'}>
+          {Object.keys(routes).map((value: string) => {
+            return routes[value].pageProps.map(
+              (page: PageProps, index: number) => {
+                return (
+                  <Route
+                    key={index}
+                    exact
+                    path={page.path}
+                    component={page.routerComponent}
+                  />
+                );
+              }
+            );
+          })}
+        </div>
+      </App>
+    )
   ) : (
     <SignInScreen />
   );
