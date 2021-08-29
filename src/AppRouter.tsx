29e9824130f7fae588458ab68817ea614a1b016f
joe-afternightfall <joe@afternightfall.com>
2021-08-29 09:12:15 -0500

    updating for new app

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,55 +1,36 @@
-import App from './App';
 import React from 'react';
+import MobileApp from './MobileApp';
 import { Route } from 'react-router';
 import { connect } from 'react-redux';
 import { State } from './configs/redux/store';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { PageProps, routes } from './configs/constants/routes';
 import { useTheme } from '@material-ui/core/styles';
-import SignInScreen from './components/top-level-components/sign-in-screen/SignInScreen';
-import MobileApp from './MobileApp';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { mobileRoutes } from './configs/constants/mobile-routes';
+import SignInScreen from './components/top-level-components/sign-in-screen/SignInScreen';
+import { WORKOUT_SCREEN_PATH } from './configs/constants/app';
+import WorkoutScreen from './components/top-level-components/workout-screen/WorkoutScreenConnector';
 
 const AppRouter = (props: AppRouterProps): JSX.Element => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
 
   return props.isValidated ? (
-    isMobile ? (
-      <MobileApp>
-        <div className={'route'}>
-          {Object.keys(mobileRoutes).map((value: string, index: number) => {
-            return (
-              <Route
-                key={index}
-                exact
-                path={mobileRoutes[value].path}
-                component={mobileRoutes[value].routerComponent}
-              />
-            );
-          })}
-        </div>
-      </MobileApp>
-    ) : (
-      <App>
-        <div className={'route'}>
-          {Object.keys(routes).map((value: string) => {
-            return routes[value].pageProps.map(
-              (page: PageProps, index: number) => {
-                return (
-                  <Route
-                    key={index}
-                    exact
-                    path={page.path}
-                    component={page.routerComponent}
-                  />
-                );
-              }
-            );
-          })}
-        </div>
-      </App>
-    )
+    <MobileApp>
+      <div className={'route'}>
+        {Object.keys(mobileRoutes).map((value: string, index: number) => {
+          return (
+            <Route
+              key={index}
+              exact
+              path={mobileRoutes[value].path}
+              component={mobileRoutes[value].routerComponent}
+            />
+          );
+        })}
+
+        <Route exact path={WORKOUT_SCREEN_PATH} component={WorkoutScreen} />
+      </div>
+    </MobileApp>
   ) : (
     <SignInScreen />
   );
--- src/reducers/application.ts
+++ src/reducers/application.ts
@@ -1,5 +1,4 @@
 import { v4 as uuidv4 } from 'uuid';
-import { RouteProp } from '../configs/constants/routes';
 import { LOCATION_CHANGE } from 'connected-react-router';
 import { getPageInfo } from '../utils/get-current-page-info';
 import {
@@ -14,6 +13,7 @@ import { UserProfileVO } from '../configs/models/UserProfileVO';
 import { WorkoutVO } from '../configs/models/WorkoutVO';
 import { StopwatchState } from '../components/top-level-components/workout-screen/stopwatch/Stopwatch';
 import { CircuitTemplateVO } from '../configs/models/CircuitTemplateVO';
+import { MobileRouteProp } from '../configs/constants/mobile-routes';
 
 function findCircuit(
   state: ApplicationState,
@@ -55,21 +55,6 @@ export default {
         newState.userProfile = action.userProfile;
         newState.setupNewUser = false;
         newState.openUserProfileDialog = false;
-        break;
-      case ActionTypes.CLOSE_SIDE_DRAWER:
-        newState.sideDrawerIsOpen = false;
-        break;
-      case ActionTypes.OPEN_SIDE_DRAWER:
-        newState.sideDrawerIsOpen = true;
-        break;
-      case ActionTypes.SET_DRAWER_SIZE:
-        newState.drawerSize = action.size;
-        break;
-      case ActionTypes.USER_CLICKED_CLOSE_DRAWER:
-        newState.userClickedCloseDrawer = true;
-        break;
-      case ActionTypes.USER_CLICKED_OPEN_DRAWER:
-        newState.userClickedCloseDrawer = false;
         break;
       case ActionTypes.TOGGLE_USER_PROFILE_DIALOG:
         newState.openUserProfileDialog = action.shouldOpen;
@@ -442,19 +427,16 @@ export default {
 };
 
 export interface ApplicationState {
+  currentLocation: string;
+  activePage: MobileRouteProp | undefined;
   userIsValidated: boolean;
   userEmail: string;
   circuitTemplates: CircuitTemplateVO[];
+  // todo: rename userWorkouts to "pastWorkouts"
   userWorkouts: WorkoutVO[];
   setupNewUser: boolean;
-  drawerSize: string;
-  userClickedCloseDrawer: boolean;
-  sideDrawerIsOpen: boolean;
-  sideDrawerIsClosed: boolean;
   userProfile: UserProfileVO | null;
   openUserProfileDialog: boolean;
-  currentLocation: string;
-  activePage: RouteProp | undefined;
   expandedAccordion: string;
   workoutConfigurations: {
     exerciseTypes: ExerciseTypeVO[];
