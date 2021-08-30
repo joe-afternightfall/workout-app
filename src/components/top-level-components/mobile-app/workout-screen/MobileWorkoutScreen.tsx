fc0a0e081e9118daea448830ce06bc78282eb874
joe-afternightfall <joe@afternightfall.com>
2021-08-30 14:47:18 -0500

    cleanup

--- src/components/top-level-components/mobile-app/workout-screen/MobileWorkoutScreen.tsx
+++ src/components/top-level-components/mobile-app/workout-screen/MobileWorkoutScreen.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import WorkoutSwipeableViews from './views/WorkoutSwipeableViews';
 import MessageAppBar from './views/components/MessageAppBar';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  useTheme,
+} from '@material-ui/core/styles';
+import SwipeableViews from 'react-swipeable-views';
+import WorkoutSelectionList from './views/1-workout-selection/WorkoutSelectionList';
+import RoutineSelectionList from './views/2-routine-selection/RoutineSelectionList';
+import Typography from '@material-ui/core/Typography';
+import FolderList from './views/3-preview-list/TossPreview';
+import TabPanel from './views/components/TabPanel';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -11,11 +21,53 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default function MobileWorkoutScreen(): JSX.Element {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
 
   return (
     <Grid container justify={'center'}>
       <Grid item xs={12}>
-        <WorkoutSwipeableViews />
+        <div>
+          <MessageAppBar
+            activeTab={value}
+            clickHandler={() => {
+              const newTab = value - 1;
+              handleChangeIndex(newTab);
+            }}
+          />
+
+          <div className={classes.toolbar} />
+
+          <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              <WorkoutSelectionList
+                goForwardHandler={() => {
+                  handleChangeIndex(1);
+                }}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+              <RoutineSelectionList
+                goForwardHandler={() => {
+                  handleChangeIndex(2);
+                }}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+              <Typography>{'tab-panel role'}</Typography>
+              <FolderList />
+            </TabPanel>
+          </SwipeableViews>
+        </div>
       </Grid>
     </Grid>
   );
