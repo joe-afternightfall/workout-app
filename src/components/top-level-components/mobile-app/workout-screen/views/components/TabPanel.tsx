e2b58bfd203cb6c6b179e0cada5da7a3ec2fa042
joe-afternightfall <joe@afternightfall.com>
2021-08-31 17:16:17 -0500

    tab panel cleanup

--- src/components/top-level-components/mobile-app/workout-screen/views/components/TabPanel.tsx
+++ src/components/top-level-components/mobile-app/workout-screen/views/components/TabPanel.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
 
 export default function TabPanel(props: TabPanelProps): JSX.Element {
   const { children, value, index, ...other } = props;
 
   return (
     <div role={'tab-panel'} hidden={value !== index} {...other}>
-      {value === index && (
-        <Box>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box component={'div'}>{children}</Box>}
     </div>
   );
 }
