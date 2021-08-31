d03c0a04c3c68fee9babf9bbcaf8b2253a126cac
joe-afternightfall <joe@afternightfall.com>
2021-08-31 17:14:58 -0500

    building on preview list

--- src/components/top-level-components/mobile-app/workout-screen/views/3-preview-list/TossPreview.tsx
+++ src/components/top-level-components/mobile-app/workout-screen/views/3-preview-list/TossPreview.tsx
@@ -3,8 +3,16 @@ import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { Divider, Grid, ListItemIcon } from '@material-ui/core';
+import {
+  Button,
+  Divider,
+  Grid,
+  ListItemIcon,
+  Typography,
+} from '@material-ui/core';
 import LinkIcon from '@material-ui/icons/Link';
+import barbellIcon from '../../../../../../configs/icons/barbell.gif';
+import dumbBellIcon from '../../../../../../configs/icons/dumbbell.png';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,93 +23,131 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function FolderList(): JSX.Element {
+interface Props {
+  goForwardHandler: () => void;
+}
+
+export default function FolderList(props: Props): JSX.Element {
   const classes = useStyles();
 
   return (
-    <List className={classes.root}>
-      <ListItem style={{ paddingLeft: 0 }}>
-        <ListItemIcon
-          style={{ backgroundColor: 'orange', height: 56, marginRight: 8 }}
+    <>
+      <List className={classes.root}>
+        <ListItem style={{ paddingLeft: 0 }}>
+          <ListItemIcon
+            style={{ backgroundColor: 'orange', height: 56, marginRight: 8 }}
+          />
+          <ListItemText
+            primary={'Downward Dog'}
+            secondary={'5 reps | 5 reps | 5 reps'}
+          />
+        </ListItem>
+        <ListItem
+          style={{ width: '100%', padding: 4, backgroundColor: 'black' }}
         />
-        <ListItemText
-          primary={'Downward Dog'}
-          secondary={'5 reps | 5 reps | 5 reps'}
-        />
-      </ListItem>
-      <ListItem
-        style={{ width: '100%', padding: 4, backgroundColor: 'black' }}
-      />
-      <ListItem style={{ padding: 0 }}>
-        <Grid container>
-          <Grid item>
-            <Grid
-              container
-              direction="column"
-              justify="center"
-              alignItems="stretch"
-              style={{ height: '100%', width: 56 }}
-            >
-              <Grid item>
-                <div
-                  style={{
-                    backgroundColor: 'blue',
-                    height: 56,
-                    width: '100%',
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <div
-                  style={{ backgroundColor: 'red', height: 56, width: '100%' }}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={10} style={{ paddingLeft: 16 }}>
-            <Grid container>
-              <Grid item xs={12}>
-                <ListItemText
-                  primary={'Burpee'}
-                  secondary={'10 reps | 8 reps | 6 reps | 4 reps'}
-                />
-              </Grid>
+        <ListItem style={{ padding: 0 }}>
+          <Grid container>
+            <Grid item>
               <Grid
-                item
-                xs={12}
                 container
-                alignItems={'center'}
-                style={{ height: 40 }}
+                direction="column"
+                justify="center"
+                alignItems="stretch"
+                style={{ height: '100%', width: 56 }}
               >
-                <Grid item xs={10}>
-                  <Divider variant={'fullWidth'} />
+                <Grid item>
+                  <div
+                    style={{
+                      backgroundColor: 'blue',
+                      height: 81,
+                      width: '100%',
+                    }}
+                  />
                 </Grid>
-                <Grid item xs={2} container alignItems={'center'}>
-                  <LinkIcon style={{ marginLeft: 8 }} />
+                <Grid item>
+                  <div
+                    style={{
+                      backgroundColor: 'red',
+                      height: 75,
+                      width: '100%',
+                    }}
+                  />
                 </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <ListItemText
-                  primary={'Mountain Climbers'}
-                  secondary={'20 reps | 18 reps | 16 reps | 14 reps'}
-                />
+            </Grid>
+            <Grid item xs={10} style={{ paddingLeft: 16 }}>
+              <Grid container>
+                <Grid item xs={12} container>
+                  <Grid item xs={12}>
+                    <Typography color={'textPrimary'} variant={'body1'}>
+                      {'Burpee'}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography color={'textSecondary'} variant={'body2'}>
+                      {'10 reps | 8 reps | 6 reps | 4 reps'}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} container alignItems={'center'}>
+                    <img
+                      style={{ height: 18, marginTop: 6 }}
+                      src={barbellIcon}
+                      alt={'barbell'}
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    container
+                    alignItems={'center'}
+                    style={{ marginTop: -4 }}
+                  >
+                    <Grid item xs={10}>
+                      <Divider variant={'fullWidth'} />
+                    </Grid>
+                    <Grid item xs={2} container alignItems={'center'}>
+                      <LinkIcon style={{ marginLeft: 8 }} />
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ListItemText
+                      primary={'Mountain Climbers'}
+                      secondary={'20 reps | 18 reps | 16 reps | 14 reps'}
+                    />
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </ListItem>
-      <ListItem
-        style={{ width: '100%', padding: 4, backgroundColor: 'black' }}
-      />
-      <ListItem style={{ paddingLeft: 0 }}>
-        <ListItemIcon
-          style={{ backgroundColor: 'gray', height: 56, marginRight: 8 }}
+        </ListItem>
+        <ListItem
+          style={{ width: '100%', padding: 4, backgroundColor: 'black' }}
         />
-        <ListItemText
-          primary={'Crunch'}
-          secondary={'20 reps | 20 reps | 20 reps'}
-        />
-      </ListItem>
-    </List>
+        <ListItem style={{ paddingLeft: 0 }}>
+          <ListItemIcon
+            style={{ backgroundColor: 'gray', height: 56, marginRight: 8 }}
+          />
+          <ListItemText
+            primary={'Crunch'}
+            secondary={
+              <Grid item xs={12} container alignItems={'center'}>
+                <Grid item xs={12}>
+                  <img
+                    style={{ height: 18, marginTop: 6 }}
+                    src={dumbBellIcon}
+                    alt={'dumbBellIcon'}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  {'20 reps | 20 reps | 20 reps'}
+                </Grid>
+              </Grid>
+            }
+          />
+        </ListItem>
+      </List>
+
+      <Button onClick={props.goForwardHandler}>{'Start Workout'}</Button>
+    </>
   );
 }
