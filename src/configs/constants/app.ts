f09903359df95fdb91fd2bc66836e7e277db73aa
joe-afternightfall <joe@afternightfall.com>
2021-09-01 10:59:37 -0500

    adding to consts

--- src/configs/constants/app.ts
+++ src/configs/constants/app.ts
@@ -5,6 +5,7 @@ export const USER_PROFILE_SCREEN_PATH = '/user-profile';
 
 export const MOBILE_EXERCISES_SCREEN_PATH = '/mobile/exercises';
 export const MOBILE_WORKOUT_SCREEN_PATH = '/mobile/workout';
+export const MOBILE_ACTIVE_WORKOUT_SCREEN_PATH = '/mobile/active-workout';
 export const MOBILE_PROFILE_PATH = '/mobile/profile';
 
 export const NUMBERS_ONLY_REGEX = new RegExp('^[0-9]*$');
