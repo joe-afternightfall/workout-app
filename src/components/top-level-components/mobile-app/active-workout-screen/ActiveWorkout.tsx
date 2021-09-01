0029d72d3d847c519f4924e107a83b486be7a6a7
joe-afternightfall <joe@afternightfall.com>
2021-09-01 10:34:28 -0500

    active workout slide in

---
+++ src/components/top-level-components/mobile-app/active-workout-screen/ActiveWorkout.tsx
@@ -0,0 +1,237 @@
+import React from 'react';
+import {
+  Button,
+  Card,
+  Divider,
+  Grid,
+  TextField,
+  Typography,
+  ListItemText,
+  Slide,
+  Toolbar,
+  IconButton,
+  AppBar,
+} from '@material-ui/core';
+import LinkIcon from '@material-ui/icons/Link';
+import barbellIcon from '../../../../configs/icons/barbell.gif';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { DASHBOARD_SCREEN_PATH } from '../../../../configs/constants/app';
+import { routerActions } from 'connected-react-router';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    toolbar: {
+      height: '10vh',
+    },
+    menuButton: {
+      paddingTop: 8,
+      paddingBottom: 8,
+    },
+    gridWrapper: {
+      height: '100%',
+    },
+  })
+);
+
+const ActiveWorkout = (props: ActiveWorkoutProps): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <Grid style={{ height: '100%' }}>
+      <>
+        <AppBar position={'absolute'}>
+          <Toolbar className={classes.toolbar}>
+            <Grid
+              container
+              className={classes.gridWrapper}
+              alignItems={'center'}
+            >
+              <Grid item xs={2}>
+                <Button onClick={props.exitClickHandler}>{'exit'}</Button>
+              </Grid>
+
+              <Grid
+                item
+                xs={8}
+                container
+                justify={'center'}
+                alignItems={'center'}
+              >
+                <Grid item>
+                  <Typography variant={'overline'}>{'1/10'}</Typography>
+                </Grid>
+              </Grid>
+
+              <Grid item xs={2} />
+            </Grid>
+          </Toolbar>
+        </AppBar>
+
+        <div className={classes.toolbar} />
+
+        <Slide mountOnEnter unmountOnExit in={true} direction={'up'}>
+          <Grid
+            container
+            direction={'column'}
+            alignItems={'stretch'}
+            justify={'space-around'}
+          >
+            <Grid item xs={12} container>
+              <Grid item>
+                <Grid
+                  container
+                  direction="column"
+                  justify="center"
+                  alignItems="stretch"
+                  style={{ height: '100%', width: 56 }}
+                >
+                  <Grid item>
+                    <div
+                      style={{
+                        backgroundColor: 'blue',
+                        height: 81,
+                        width: '100%',
+                      }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <div
+                      style={{
+                        backgroundColor: 'red',
+                        height: 75,
+                        width: '100%',
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={10} style={{ paddingLeft: 16 }}>
+                <Grid container>
+                  <Grid item xs={12} container>
+                    <Grid item xs={12}>
+                      <Typography color={'textPrimary'} variant={'body1'}>
+                        {'Burpee'}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography color={'textSecondary'} variant={'body2'}>
+                        {'10 reps | 8 reps | 6 reps | 4 reps'}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12} container alignItems={'center'}>
+                      <img
+                        style={{ height: 18, marginTop: 6 }}
+                        src={barbellIcon}
+                        alt={'barbell'}
+                      />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={12}
+                      container
+                      alignItems={'center'}
+                      style={{ marginTop: -4 }}
+                    >
+                      <Grid item xs={10}>
+                        <Divider variant={'fullWidth'} />
+                      </Grid>
+                      <Grid item xs={2} container alignItems={'center'}>
+                        <LinkIcon style={{ marginLeft: 8 }} />
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ListItemText
+                        primary={'Mountain Climbers'}
+                        secondary={'20 reps | 18 reps | 16 reps | 14 reps'}
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Grid container>
+                <Grid item xs={8}>
+                  <TextField fullWidth variant={'outlined'} value={'10 reps'} />
+                  <TextField fullWidth variant={'outlined'} value={'20 reps'} />
+                </Grid>
+                <Grid item xs={2}>
+                  <Button>{'Did It'}</Button>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12} container>
+              <Card>
+                <Grid item xs={12} container>
+                  <Grid item xs={12}>
+                    <Typography color={'textPrimary'} variant={'body1'}>
+                      {'Up Next'}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography color={'textPrimary'} variant={'body1'}>
+                      {'Burpee'}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography color={'textSecondary'} variant={'body2'}>
+                      {'10 reps | 8 reps | 6 reps | 4 reps'}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} container alignItems={'center'}>
+                    <img
+                      style={{ height: 18, marginTop: 6 }}
+                      src={barbellIcon}
+                      alt={'barbell'}
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    container
+                    alignItems={'center'}
+                    style={{ marginTop: -4 }}
+                  >
+                    <Grid item xs={10}>
+                      <Divider variant={'fullWidth'} />
+                    </Grid>
+                    <Grid item xs={2} container alignItems={'center'}>
+                      <LinkIcon style={{ marginLeft: 8 }} />
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ListItemText
+                      primary={'Mountain Climbers'}
+                      secondary={'20 reps | 18 reps | 16 reps | 14 reps'}
+                    />
+                  </Grid>
+                </Grid>
+              </Card>
+            </Grid>
+          </Grid>
+        </Slide>
+      </>
+    </Grid>
+  );
+};
+
+export interface ActiveWorkoutProps {
+  exitClickHandler: () => void;
+}
+
+const mapStateToProps = (state: any): ActiveWorkoutProps => {
+  return {} as unknown as ActiveWorkoutProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): ActiveWorkoutProps =>
+  ({
+    exitClickHandler: () => {
+      dispatch(routerActions.push(DASHBOARD_SCREEN_PATH));
+    },
+  } as unknown as ActiveWorkoutProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActiveWorkout);
