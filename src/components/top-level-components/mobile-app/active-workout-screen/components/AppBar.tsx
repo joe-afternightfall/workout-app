92975752eabfc2584b2e95c9a10eb01e240d9c70
joe-afternightfall <joe@afternightfall.com>
2021-09-04 00:32:07 -0500

    app bar

---
+++ src/components/top-level-components/mobile-app/active-workout-screen/components/AppBar.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+  Grid,
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import ListIcon from '@material-ui/icons/List';
+import { routerActions } from 'connected-react-router';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { DASHBOARD_SCREEN_PATH } from '../../../../../configs/constants/app';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    toolbar: {
+      padding: 0,
+      height: '8vh',
+    },
+    menuButton: {
+      paddingBottom: 8,
+      color: '#ED440B',
+    },
+    exitButton: {
+      color: '#ed440b',
+    },
+    gridRoot: {
+      height: '100%',
+    },
+    titleWrapper: {
+      paddingBottom: 2,
+    },
+  })
+);
+
+const ActiveWorkoutAppBar = (props: ActiveWorkoutAppBarProps): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position={'absolute'} color={'transparent'} elevation={0}>
+      <Toolbar className={classes.toolbar}>
+        <Grid
+          container
+          justify={'center'}
+          alignItems={'flex-end'}
+          className={classes.gridRoot}
+        >
+          <Grid item xs={2}>
+            <Button
+              className={classes.exitButton}
+              onClick={props.exitClickHandler}
+            >
+              {'exit'}
+            </Button>
+          </Grid>
+
+          <Grid
+            item
+            xs={8}
+            container
+            justify={'center'}
+            alignItems={'flex-end'}
+            className={classes.titleWrapper}
+          >
+            <Typography variant={'subtitle1'}>{'1/10'}</Typography>
+          </Grid>
+
+          <Grid
+            item
+            xs={2}
+            container
+            justify={'flex-end'}
+            alignItems={'flex-end'}
+          >
+            <IconButton className={classes.menuButton} color={'inherit'}>
+              <ListIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export interface ActiveWorkoutAppBarProps {
+  exitClickHandler: () => void;
+}
+
+const mapStateToProps = (): ActiveWorkoutAppBarProps => {
+  return {} as unknown as ActiveWorkoutAppBarProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): ActiveWorkoutAppBarProps =>
+  ({
+    exitClickHandler: () => {
+      dispatch(routerActions.push(DASHBOARD_SCREEN_PATH));
+    },
+  } as unknown as ActiveWorkoutAppBarProps);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ActiveWorkoutAppBar);
