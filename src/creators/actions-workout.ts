9e6c08745b1b51900abd9e6fc04fc2f8ca3c6da9
joe-afternightfall <joe@afternightfall.com>
2021-09-06 18:01:20 -0500

    adding to workout types

--- src/creators/actions-workout.ts
+++ src/creators/actions-workout.ts
@@ -1,11 +1,19 @@
 import { LocationChangeAction } from 'connected-react-router';
 import { InitializeWorkoutConfigsAction } from './initialize';
+import {
+  SelectedRoutineAction,
+  SelectedWorkoutCategoryAction,
+} from './new-workout/workout-selections';
 
 export enum WorkoutActionTypes {
   // Workout Actions
   INITIALIZE = 'INITIALIZE',
+  SELECTED_WORKOUT_CATEGORY = 'SELECTED_WORKOUT_CATEGORY',
+  SELECTED_ROUTINE = 'SELECTED_ROUTINE',
 }
 
 export type WorkoutActions =
+  | LocationChangeAction
   | InitializeWorkoutConfigsAction
-  | LocationChangeAction;
+  | SelectedWorkoutCategoryAction
+  | SelectedRoutineAction;
