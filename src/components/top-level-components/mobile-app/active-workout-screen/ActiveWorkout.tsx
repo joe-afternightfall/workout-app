a99699a1ca8006743fc145c3fcc89db24d64db74
joe-afternightfall <joe@afternightfall.com>
2021-09-06 21:26:18 -0500

    updating

--- src/components/top-level-components/mobile-app/active-workout-screen/ActiveWorkout.tsx
+++ src/components/top-level-components/mobile-app/active-workout-screen/ActiveWorkout.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 import { scroller } from 'react-scroll';
 import { Grid, Slide } from '@material-ui/core';
 import ActiveSet from './2-active-set/ActiveSet';
@@ -6,6 +8,16 @@ import UpNextCard from './3-up-next-card/UpNextCard';
 import ActiveWorkoutAppBar from './components/AppBar';
 import ActiveExercise from './1-active-exercise/ActiveExercise';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { State } from '../../../../configs/redux/store';
+import { WorkoutDAO } from '../../../../configs/models/workout/WorkoutDAO';
+import {
+  Phase,
+  Set,
+  Segment,
+  WorkoutExercise,
+} from '../../../../configs/models/AppInterfaces';
+import { ExerciseVO } from '../../../../configs/models/configurations/ExerciseVO';
+import { getExerciseName, isSuperset } from '../../../../utils/active-workout';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -15,8 +27,16 @@ const useStyles = makeStyles(() =>
   })
 );
 
-export default function ActiveWorkout(): JSX.Element {
+const ActiveWorkout = (props: ActiveWorkoutProps): JSX.Element => {
   const classes = useStyles();
+  const [currentPhase, setCurrentPhase] = useState<Phase>(
+    props.activeWorkout.routine.phases[0]
+  );
+  const [currentSegment, setCurrentSegment] = useState<Segment>(
+    currentPhase.segments[0]
+  );
+
+  const superset = isSuperset(currentSegment.trainingSetTypeId);
 
   const scrollToSection = () => {
     scroller.scrollTo('third-set-row', {
@@ -25,20 +45,34 @@ export default function ActiveWorkout(): JSX.Element {
       smooth: 'easeInOutQuart',
     });
   };
+  const totalSegments = currentPhase.segments.length;
 
   return (
     <Slide mountOnEnter unmountOnExit in={true} direction={'up'}>
       <div>
-        <ActiveWorkoutAppBar />
+        <ActiveWorkoutAppBar currentPhase={'blah'} />
 
         <div className={classes.toolbarMixin} />
 
         <Grid container style={{ height: '87vh' }}>
           <Grid item xs={12}>
-            <ActiveExercise exercises={[{ title: 'Mountain Climbers' }]} />
+            <ActiveExercise
+              superset={superset}
+              exerciseTitles={currentSegment.exercises.map(
+                (exercise: WorkoutExercise) => {
+                  return {
+                    title: getExerciseName(
+                      props.allExercises,
+                      exercise.exerciseId
+                    ),
+                  };
+                }
+              )}
+            />
           </Grid>
 
           <Grid item xs={12}>
+            {currentSegment.trainingSetTypeId}
             <ActiveSet didItClickHandler={() => alert('clicked')} currentSet />
           </Grid>
 
@@ -62,4 +96,21 @@ export default function ActiveWorkout(): JSX.Element {
       </div>
     </Slide>
   );
+};
+
+export interface ActiveWorkoutProps {
+  activeWorkout: WorkoutDAO;
+  allExercises: ExerciseVO[];
 }
+
+const mapStateToProps = (state: State): ActiveWorkoutProps => {
+  return {
+    activeWorkout: state.workoutState.activeWorkout,
+    allExercises: state.workoutState.configs.exercises,
+  } as unknown as ActiveWorkoutProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): ActiveWorkoutProps =>
+  ({} as unknown as ActiveWorkoutProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActiveWorkout);
