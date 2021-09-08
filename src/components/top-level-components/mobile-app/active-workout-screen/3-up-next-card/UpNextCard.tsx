7a4765cc19f485a46a4f9bcbd4f01086e8edc39b
joe-afternightfall <joe@afternightfall.com>
2021-09-08 17:38:32 -0500

    up next setup

--- src/components/top-level-components/mobile-app/active-workout-screen/3-up-next-card/UpNextCard.tsx
+++ src/components/top-level-components/mobile-app/active-workout-screen/3-up-next-card/UpNextCard.tsx
@@ -1,74 +1,109 @@
 import React from 'react';
+import { Grid, Card } from '@material-ui/core';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 import {
-  Grid,
-  Card,
-  Divider,
-  Typography,
-  ListItemText,
-} from '@material-ui/core';
-import LinkIcon from '@material-ui/icons/Link';
-import barbellIcon from '../../../../../configs/icons/barbell.gif';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+  Segment,
+  WorkoutExercise,
+} from '../../../../../configs/models/AppInterfaces';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { State } from '../../../../../configs/redux/store';
+import {
+  isCircuitSet,
+  isStraightSet,
+  isSuperset,
+} from '../../../../../utils/active-workout';
+import SingleListItem from '../../shared/SingleListItem';
+import {
+  buildRepsAndSets,
+  getExerciseName,
+} from '../../../../../utils/workout-configs';
+import SuperSetItem from '../../shared/SuperSetItem';
+import { ExerciseVO } from '../../../../../configs/models/configurations/ExerciseVO';
 
-const useStyles = makeStyles(() =>
+export interface UpNextCardProps {
+  nextSegment: Segment;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
+    supersetRoot: {
+      width: '100%',
+    },
+    baseRoot: {
+      width: '100%',
+    },
   })
 );
 
-export default function UpNextCard(props: UpNextCardProps): JSX.Element {
+const UpNextCard = (props: UpNextCardProps): JSX.Element => {
   const classes = useStyles();
+  let display = <div />;
+
+  const workoutExercises = props.nextSegment.exercises;
+  const straightSet = isStraightSet(props.nextSegment.trainingSetTypeId);
+  const circuitSet = isCircuitSet(props.nextSegment.trainingSetTypeId);
+  if (straightSet || circuitSet) {
+    workoutExercises.map((exercise: WorkoutExercise) => {
+      display = (
+        <SingleListItem
+          key={exercise.id}
+          exerciseTitle={getExerciseName(props.exercises, exercise.exerciseId)}
+          repsAndSets={buildRepsAndSets(exercise.sets)}
+        />
+      );
+    });
+  } else if (isSuperset(props.nextSegment.trainingSetTypeId)) {
+    display = (
+      <SuperSetItem
+        upNextTitle
+        firstExerciseTitle={getExerciseName(
+          props.exercises,
+          workoutExercises[0].exerciseId
+        )}
+        firstExerciseRepsAndSets={buildRepsAndSets(workoutExercises[0].sets)}
+        secondExerciseTitle={getExerciseName(
+          props.exercises,
+          workoutExercises[1].exerciseId
+        )}
+        secondExerciseRepsAndSets={buildRepsAndSets(workoutExercises[1].sets)}
+      />
+    );
+  }
+
+  let rootClass = '';
+  if (straightSet || circuitSet) {
+    rootClass = classes.baseRoot;
+  } else {
+    rootClass = classes.supersetRoot;
+  }
 
   return (
-    <Card>
+    <Card className={rootClass}>
       <Grid item xs={12} container>
-        <Grid item xs={12}>
-          <Typography color={'textPrimary'} variant={'body1'}>
-            {'Up Next'}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography color={'textPrimary'} variant={'body1'}>
-            {'Burpee'}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography color={'textSecondary'} variant={'body2'}>
-            {'10 reps | 8 reps | 6 reps | 4 reps'}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} container alignItems={'center'}>
-          <img
-            style={{ height: 18, marginTop: 6 }}
-            src={barbellIcon}
-            alt={'barbell'}
-          />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          container
-          alignItems={'center'}
-          style={{ marginTop: -4 }}
-        >
-          <Grid item xs={10}>
-            <Divider variant={'fullWidth'} />
-          </Grid>
-          <Grid item xs={2} container alignItems={'center'}>
-            <LinkIcon style={{ marginLeft: 8 }} />
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <ListItemText
-            primary={'Mountain Climbers'}
-            secondary={'20 reps | 18 reps | 16 reps | 14 reps'}
-          />
-        </Grid>
+        {display}
       </Grid>
     </Card>
   );
-}
+};
 
 export interface UpNextCardProps {
-  DELETE_ME?: undefined;
+  nextSegment: Segment;
+  exercises: ExerciseVO[];
 }
+
+const mapStateToProps = (state: State): UpNextCardProps => {
+  const foundSegment = state.workoutState.currentPhase.segments.find(
+    (segment) => segment.order === state.workoutState.currentSegmentIndex + 1
+  );
+
+  return {
+    nextSegment: foundSegment && foundSegment,
+    exercises: state.workoutState.configs.exercises,
+  } as unknown as UpNextCardProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): UpNextCardProps =>
+  ({} as unknown as UpNextCardProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpNextCard);
