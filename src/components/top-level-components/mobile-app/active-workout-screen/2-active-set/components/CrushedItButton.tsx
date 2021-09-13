b1f75b560799fdab8339af6b9a453d2785c906f1
joe-afternightfall <joe@afternightfall.com>
2021-09-13 10:36:28 -0500

    crushed it button setup/defect fix

--- src/components/top-level-components/mobile-app/active-workout-screen/2-active-set/components/CrushedItButton.tsx
+++ src/components/top-level-components/mobile-app/active-workout-screen/2-active-set/components/CrushedItButton.tsx
@@ -1,46 +1,31 @@
-import clsx from 'clsx';
 import React from 'react';
 import { Button } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      width: '100%',
-      height: '100%',
-      borderRadius: '0 8px 8px 0',
-      // backgroundColor: '#222323',
-    },
-    activeOrange: {
-      borderColor: '#ED440B',
-      backgroundColor: '#ED440B',
-    },
-    baseColor: {
-      borderColor: '#222323',
-      backgroundColor: '#222323',
-    },
-    done: {
-      borderColor: '#ED440B',
-      backgroundColor: '#ED440B',
-      opacity: 0.6,
-    },
-  })
-);
 
 export default function CrushedItButton(
   props: CrushedItButtonProps
 ): JSX.Element {
-  const classes = useStyles();
+  let color = '';
+
+  if (props.activeSet || props.markedDone) {
+    color = '#ED440B';
+  } else {
+    color = '#222323';
+  }
 
   return (
     <Button
-      className={clsx(
-        classes.root,
-        props.activeSet ? classes.activeOrange : classes.baseColor,
-        props.markedDone ? classes.done : undefined
-      )}
-      onClick={props.crushedItClickHandler}
+      style={{
+        backgroundColor: color,
+        opacity: props.markedDone ? 0.6 : 1,
+        color: '#FFF',
+        width: '100%',
+        height: '100%',
+        borderRadius: '0 8px 8px 0',
+        padding: '6px 0',
+      }}
+      variant={'contained'}
       disabled={props.markedDone}
+      onClick={props.crushedItClickHandler}
     >
       {'Crushed It'}
     </Button>
