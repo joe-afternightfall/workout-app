bbcc0a3aad41d9f7c751f886b8b61c7e8933d042
joe-afternightfall <joe@afternightfall.com>
2021-09-16 11:40:50 -0500

    add set action

--- src/creators/new-workout/preview-workout.ts
+++ src/creators/new-workout/preview-workout.ts
@@ -13,3 +13,17 @@ export const deleteSetFromEditingCopy = (
     setId: setId,
   };
 };
+
+export interface AddSetToEditingCopyAction {
+  type: WorkoutActionTypes.ADD_SET_TO_EDITING_COPY;
+  segmentExerciseId: string;
+}
+
+export const addSetToEditingCopy = (
+  segmentExerciseId: string
+): AddSetToEditingCopyAction => {
+  return {
+    type: WorkoutActionTypes.ADD_SET_TO_EDITING_COPY,
+    segmentExerciseId: segmentExerciseId,
+  };
+};
