56c3d1fd3ca0432052e1f7f0c7d802839ffd5fe1
joe-yarbrough <joe@afternightfall.com>
2021-11-05 19:40:26 -0500

    creating appbar

---
+++ src/components/top-level-components/mobile-app/active-workout-screen/components/app-bar/AppBar.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ExitDialog from './exit-dialog/ExitDialog';
+import { State } from '../../../../../../configs/redux/store';
+import ExerciseListDrawer from './active-exercise-list-drawer/ActiveExerciseListDrawer';
+import { getPhaseName } from '../../../../../../utils/name-finder';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Grid, AppBar, Toolbar, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    gridRoot: {
+      height: '100%',
+    },
+  })
+);
+
+const ActiveWorkoutAppBar = (props: ActiveWorkoutAppBarProps): JSX.Element => {
+  const classes = useStyles();
+  const { phaseName, currentSegmentCount } = props;
+
+  return (
+    <AppBar position={'absolute'} color={'transparent'} elevation={0}>
+      <Toolbar disableGutters>
+        <Grid container>
+          <Grid item xs={2} container justify={'center'} alignItems={'center'}>
+            <ExitDialog />
+          </Grid>
+
+          <Grid item xs={8}>
+            <Grid
+              item
+              xs={12}
+              container
+              justify={'center'}
+              alignItems={'flex-end'}
+            >
+              <Typography variant={'overline'}>{phaseName}</Typography>
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              container
+              justify={'center'}
+              alignItems={'flex-end'}
+            >
+              <Typography variant={'subtitle1'}>
+                {currentSegmentCount}
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={2} container justify={'center'} alignItems={'center'}>
+            <ExerciseListDrawer />
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+interface ActiveWorkoutAppBarProps {
+  phaseName: string;
+  currentSegmentCount: string;
+}
+
+const mapStateToProps = (state: State): ActiveWorkoutAppBarProps => {
+  const totalSegments = state.workoutState.currentPhase.segments.length;
+  const currentSegmentIndex = state.workoutState.currentSegmentIndex;
+
+  return {
+    phaseName: getPhaseName(
+      state.workoutState.configs.phases,
+      state.workoutState.currentPhase.phaseId
+    ),
+    currentSegmentCount: `${currentSegmentIndex} of ${totalSegments}`,
+  } as unknown as ActiveWorkoutAppBarProps;
+};
+
+export default connect(mapStateToProps)(ActiveWorkoutAppBar);
