416adf9f68f826115eb6a2feaed26fd7a1ac7043
joe-yarbrough <joe@afternightfall.com>
2021-11-05 20:13:03 -0500

    refactor

--- src/components/top-level-components/mobile-app/active-workout-screen/components/app-bar/AppBar.tsx
+++ src/components/top-level-components/mobile-app/active-workout-screen/components/app-bar/AppBar.tsx
@@ -5,6 +5,23 @@ import { State } from '../../../../../../configs/redux/store';
 import { getPhaseName } from '../../../../../../utils/name-finder';
 import { Grid, AppBar, Toolbar, Typography } from '@material-ui/core';
 import ExerciseListDrawer from './active-exercise-list-drawer/ActiveExerciseListDrawer';
+import { Variant } from '@material-ui/core/styles/createTypography';
+
+const endSection = (element: JSX.Element) => {
+  return (
+    <Grid item xs={2} container justify={'center'} alignItems={'center'}>
+      {element}
+    </Grid>
+  );
+};
+
+const headerTitle = (text: string, variant: Variant) => {
+  return (
+    <Grid item xs={12} container justify={'center'} alignItems={'flex-end'}>
+      <Typography variant={variant}>{text}</Typography>
+    </Grid>
+  );
+};
 
 const ActiveWorkoutAppBar = (props: ActiveWorkoutAppBarProps): JSX.Element => {
   const { phaseName, currentSegmentCount } = props;
@@ -13,37 +30,12 @@ const ActiveWorkoutAppBar = (props: ActiveWorkoutAppBarProps): JSX.Element => {
     <AppBar position={'absolute'} color={'transparent'} elevation={0}>
       <Toolbar disableGutters>
         <Grid container>
-          <Grid item xs={2} container justify={'center'} alignItems={'center'}>
-            <ExitDialog />
-          </Grid>
-
+          {endSection(<ExitDialog />)}
           <Grid item xs={8}>
-            <Grid
-              item
-              xs={12}
-              container
-              justify={'center'}
-              alignItems={'flex-end'}
-            >
-              <Typography variant={'overline'}>{phaseName}</Typography>
-            </Grid>
-
-            <Grid
-              item
-              xs={12}
-              container
-              justify={'center'}
-              alignItems={'flex-end'}
-            >
-              <Typography variant={'subtitle1'}>
-                {currentSegmentCount}
-              </Typography>
-            </Grid>
+            {headerTitle(phaseName, 'overline')}
+            {headerTitle(currentSegmentCount, 'subtitle1')}
           </Grid>
-
-          <Grid item xs={2} container justify={'center'} alignItems={'center'}>
-            <ExerciseListDrawer />
-          </Grid>
+          {endSection(<ExerciseListDrawer />)}
         </Grid>
       </Toolbar>
     </AppBar>
