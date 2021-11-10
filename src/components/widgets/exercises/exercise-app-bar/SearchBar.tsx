3b8cb35d1da901e27e517e7d6132b1bd3a64cf92
joe-yarbrough <joe@afternightfall.com>
2021-11-10 14:45:40 -0600

    adding clear

--- src/components/widgets/exercises/exercise-app-bar/SearchBar.tsx
+++ src/components/widgets/exercises/exercise-app-bar/SearchBar.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
-import React, { useState } from 'react';
+import { Close } from '@material-ui/icons';
 import SearchIcon from '@material-ui/icons/Search';
+import { State } from '../../../../configs/redux/store';
 import { Grid, IconButton, InputBase } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { filterExercisesForSearchValue } from '../../../../creators/workout/exercises';
@@ -48,27 +50,46 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const SearchBar = (props: SearchBarProps & PassedInProps): JSX.Element => {
   const classes = useStyles();
-  const { expanded, clickHandler } = props;
+  const { expanded, clickHandler, searchValue } = props;
   return (
-    <Grid item xs={12} className={classes.root} container>
-      <div className={classes.iconWrapper}>
-        <IconButton
-          onClick={clickHandler}
-          className={expanded ? classes.expandedButton : classes.closedButton}
-        >
-          <SearchIcon />
-        </IconButton>
-      </div>
-      <InputBase
-        placeholder={'Search…'}
-        classes={{
-          input: expanded ? classes.inputOverride : classes.hide,
-        }}
-        inputProps={{ 'aria-label': 'search' }}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          props.searchHandler(e.target.value);
-        }}
-      />
+    <Grid item xs={12} className={classes.root}>
+      <Grid container>
+        <Grid item xs={10}>
+          <div className={classes.iconWrapper}>
+            <IconButton
+              onClick={clickHandler}
+              className={
+                expanded ? classes.expandedButton : classes.closedButton
+              }
+            >
+              <SearchIcon />
+            </IconButton>
+          </div>
+          <InputBase
+            placeholder={'Search…'}
+            value={searchValue}
+            classes={{
+              input: expanded ? classes.inputOverride : classes.hide,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              props.searchHandler(e.target.value);
+            }}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          {expanded && (
+            <IconButton
+              onClick={() => {
+                props.searchHandler('');
+              }}
+              disabled={searchValue === ''}
+            >
+              <Close />
+            </IconButton>
+          )}
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
@@ -77,9 +98,17 @@ interface PassedInProps {
   clickHandler: () => void;
   expanded: boolean;
 }
+
 interface SearchBarProps {
+  searchValue: string;
   searchHandler: (value: string) => void;
 }
+
+const mapStateToProps = (state: State): SearchBarProps => {
+  return {
+    searchValue: state.applicationState.exerciseSearchValue,
+  } as unknown as SearchBarProps;
+};
 
 const mapDispatchToProps = (dispatch: Dispatch): SearchBarProps =>
   ({
@@ -88,4 +117,4 @@ const mapDispatchToProps = (dispatch: Dispatch): SearchBarProps =>
     },
   } as unknown as SearchBarProps);
 
-export default connect(null, mapDispatchToProps)(SearchBar);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
