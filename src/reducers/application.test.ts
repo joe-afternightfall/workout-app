183ffdc66325eb8c8b38f83e4bff2bf6a448343d
joe-yarbrough <joe@afternightfall.com>
2021-11-12 08:53:11 -0600

    adding to test

--- src/reducers/application.test.ts
+++ src/reducers/application.test.ts
@@ -1,24 +1,69 @@
-import application from './application';
+import application, { ApplicationState } from './application';
+import { ActionTypes } from '../creators/actions';
+import { buildWorkoutEquipment } from '../configs/test-utils/test-vo-builder';
 
 describe('Application Reducer', () => {
-  it('should update current location', () => {
-    const state = application.reducer(undefined, {
-      type: '@@router/LOCATION_CHANGE',
-      payload: {
-        location: {
-          pathname: '/testing-pathname',
-        },
-      },
-    });
+  // it('should update current location', () => {
+  //   const state = application.reducer(undefined, {
+  //     type: '@@router/LOCATION_CHANGE',
+  //     payload: {
+  //       location: {
+  //         pathname: '/testing-pathname',
+  //       },
+  //     },
+  //   });
+  //
+  //   expect(state.currentLocation).toBe('/testing-pathname');
+  // });
+  //
+  // it('should return empty object', () => {
+  //   const state = application.reducer(undefined, {
+  //     type: 'TESTING',
+  //   });
+  //
+  //   expect(state).toEqual({});
+  // });
 
-    expect(state.currentLocation).toBe('/testing-pathname');
+  it('should return VALIDATED_USER', function () {
+    const state = application.reducer({} as unknown as ApplicationState, {
+      type: ActionTypes.VALIDATED_USER,
+      email: 'test-email',
+    });
+    expect(state.userIsValidated).toEqual(true);
+    expect(state.userEmail).toEqual('test-email');
   });
 
-  it('should return empty object', () => {
-    const state = application.reducer(undefined, {
-      type: 'TESTING',
+  it('should return SETUP_NEW_USER action', function () {
+    const state = application.reducer({} as unknown as ApplicationState, {
+      type: ActionTypes.SETUP_NEW_USER,
+      email: 'test-email',
     });
+    expect(state.setupNewUser).toEqual(true);
+    expect(state.userEmail).toEqual('test-email');
+  });
 
-    expect(state).toEqual({});
+  // it('should return LOAD_USER_INFO action', function () {
+  //   const state = application.reducer({} as unknown as ApplicationState, {
+  //     type: ActionTypes.LOAD_USER_INFO,
+  //     email: 'test-email',
+  //   });
+  //   expect(state.setupNewUser).toEqual(false);
+  //   expect(state.openUserProfileDialog).toEqual(false);
+  // });
+
+  it('should return LOAD_WORKOUT_EQUIPMENT action', function () {
+    const state = application.reducer(
+      {
+        workoutConfigurations: {
+          workoutEquipment: [],
+        },
+      } as unknown as ApplicationState,
+      {
+        type: ActionTypes.LOAD_WORKOUT_EQUIPMENT,
+        workoutEquipment: buildWorkoutEquipment(3),
+      }
+    );
+
+    expect(state.workoutConfigurations.workoutEquipment.length).toEqual(3);
   });
 });
