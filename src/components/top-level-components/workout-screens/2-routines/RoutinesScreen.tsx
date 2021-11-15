bd7f428d31343b981b1854e942b7fa36226585dc
joe-yarbrough <joe@afternightfall.com>
2021-11-15 08:11:17 -0600

    base setup

--- src/components/top-level-components/workout-screens/2-routines/RoutinesScreen.tsx
+++ src/components/top-level-components/workout-screens/2-routines/RoutinesScreen.tsx
@@ -1,37 +1,42 @@
-import { RoutineTemplateVO } from 'workout-app-common-core';
-import { Grid } from '@material-ui/core';
-import SelectionCard from '../shared-components/SelectionCard';
-import { isOdd } from '../../../../utils/number-util';
 import React from 'react';
-import { State } from '../../../../configs/redux/store';
+import { connect } from 'react-redux';
+import { Grid } from '@material-ui/core';
 import { AnyAction, Dispatch } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
-import { getExerciseImagesForRoutine } from '../../../../services/exercise-images';
-import { selectedRoutine } from '../../../../creators/workout/workout-selections';
-import { connect } from 'react-redux';
+import { isOdd } from '../../../../utils/number-util';
 import { routerActions } from 'connected-react-router';
+import { State } from '../../../../configs/redux/store';
+import { RoutineTemplateVO } from 'workout-app-common-core';
+import SelectionCard from '../shared-components/SelectionCard';
+import BaseWorkoutScreen from '../shared-components/BaseWorkoutScreen';
+import { selectedRoutine } from '../../../../creators/workout/workout-selections';
+import { getExerciseImagesForRoutine } from '../../../../services/exercise-images';
 import { PREVIEW_WORKOUT_SCREEN_PATH } from '../../../../configs/constants/app-routing';
 
 const RoutinesScreen = (props: RoutinesScreenProps): JSX.Element => {
   return (
-    <Grid container justify={'center'}>
-      {props.routinesForCategory.map(
-        (routine: RoutineTemplateVO, index: number) => {
-          return (
-            <Grid key={routine.id} item xs={12}>
-              <SelectionCard
-                title={routine.name}
-                addTopMargin={index > 0}
-                clickHandler={() => {
-                  props.selectRoutineHandler(routine);
-                }}
-                justify={isOdd(index) ? 'flex-end' : 'flex-start'}
-              />
-            </Grid>
-          );
-        }
-      )}
-    </Grid>
+    <BaseWorkoutScreen
+      centerComponent={
+        <Grid container justify={'center'}>
+          {props.routinesForCategory.map(
+            (routine: RoutineTemplateVO, index: number) => {
+              return (
+                <Grid key={routine.id} item xs={12}>
+                  <SelectionCard
+                    title={routine.name}
+                    addTopMargin={index > 0}
+                    clickHandler={() => {
+                      props.selectRoutineHandler(routine);
+                    }}
+                    justify={isOdd(index) ? 'flex-end' : 'flex-start'}
+                  />
+                </Grid>
+              );
+            }
+          )}
+        </Grid>
+      }
+    />
   );
 };
 
