26ad1d9d22b4332845b9990e9e1894a7adeeb4ac
joe-yarbrough <joe@afternightfall.com>
2021-11-15 08:50:51 -0600

    base cleanup

--- src/components/top-level-components/workout-screens/shared-components/BaseWorkoutScreen.tsx
+++ src/components/top-level-components/workout-screens/shared-components/BaseWorkoutScreen.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
-import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { Grid } from '@material-ui/core';
 import { State } from '../../../../configs/redux/store';
-import { routerActions } from 'connected-react-router';
-import { WorkoutCategoryVO } from 'workout-app-common-core';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
 import EditSet from '../3-preview-workout/components/edit-set/EditSet';
 import WorkoutScreensAppBar from '../shared-components/WorkoutScreensAppBar';
-import { ROUTINES_SCREEN_PATH } from '../../../../configs/constants/app-routing';
-import { selectedWorkoutCategory } from '../../../../creators/workout/workout-selections';
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    toolbar: {
-      height: '7vh',
-    },
-  })
-);
 
 const BaseWorkoutScreen = (
   props: BaseWorkoutScreenProps & PassedInProps
 ): JSX.Element => {
-  const classes = useStyles();
   const { centerComponent } = props;
 
   return (
     <Grid container justify={'center'}>
       <Grid item xs={12}>
-        {!props.displayEditSet && (
-          <>
-            <WorkoutScreensAppBar />
-            <div className={classes.toolbar} />
-          </>
-        )}
+        {!props.displayEditSet && <WorkoutScreensAppBar />}
 
         {centerComponent}
 
-        {/*// todo: rip <EditSet /> out to app shell*/}
         {props.displayEditSet && <EditSet />}
       </Grid>
     </Grid>
@@ -50,24 +29,12 @@ interface PassedInProps {
 
 interface BaseWorkoutScreenProps {
   displayEditSet: boolean;
-  workoutCategories: WorkoutCategoryVO[];
-  selectWorkoutCategoryHandler: (category: WorkoutCategoryVO) => void;
 }
 
 const mapStateToProps = (state: State): BaseWorkoutScreenProps => {
   return {
     displayEditSet: state.workoutState.displayEditSet,
-    workoutCategories:
-      state.applicationState.workoutConfigurations.workoutCategories,
   } as unknown as BaseWorkoutScreenProps;
 };
 
-const mapDispatchToProps = (dispatch: Dispatch): BaseWorkoutScreenProps =>
-  ({
-    selectWorkoutCategoryHandler: (category: WorkoutCategoryVO) => {
-      dispatch(selectedWorkoutCategory(category));
-      dispatch(routerActions.push(ROUTINES_SCREEN_PATH));
-    },
-  } as unknown as BaseWorkoutScreenProps);
-
-export default connect(mapStateToProps, mapDispatchToProps)(BaseWorkoutScreen);
+export default connect(mapStateToProps)(BaseWorkoutScreen);
