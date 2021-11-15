4e5683d74e7f9b0c9a5151329128aeb7608bdfc2
joe-yarbrough <joe@afternightfall.com>
2021-11-15 08:16:08 -0600

    setup with base

--- src/components/top-level-components/workout-screens/1-all-workouts/AllWorkoutsScreen.tsx
+++ src/components/top-level-components/workout-screens/1-all-workouts/AllWorkoutsScreen.tsx
@@ -7,61 +7,34 @@ import { State } from '../../../../configs/redux/store';
 import { routerActions } from 'connected-react-router';
 import { WorkoutCategoryVO } from 'workout-app-common-core';
 import SelectionCard from '../shared-components/SelectionCard';
-import WorkoutScreensAppBar from '../shared-components/WorkoutScreensAppBar';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import EditSet from '../3-preview-workout/components/edit-set/EditSet';
+import BaseWorkoutScreen from '../shared-components/BaseWorkoutScreen';
 import { ROUTINES_SCREEN_PATH } from '../../../../configs/constants/app-routing';
 import { selectedWorkoutCategory } from '../../../../creators/workout/workout-selections';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    toolbar: {
-      height: '7vh',
-    },
-  })
-);
-
 const AllWorkoutsScreen = (props: AllWorkoutsScreenProps): JSX.Element => {
-  const classes = useStyles();
-  const [activeIndex, setActiveIndex] = React.useState<number>(0);
-
-  const goBack = () => {
-    setActiveIndex(activeIndex - 1);
-  };
-
   return (
-    <Grid container justify={'center'}>
-      <Grid item xs={12}>
-        {!props.displayEditSet && (
-          <>
-            <WorkoutScreensAppBar
-              activeTab={activeIndex}
-              clickHandler={goBack}
-            />
-            <div className={classes.toolbar} />
-          </>
-        )}
-
-        {props.workoutCategories.map(
-          (category: WorkoutCategoryVO, index: number) => {
-            return (
-              <Grid key={category.id} item xs={12}>
-                <SelectionCard
-                  title={category.name}
-                  addTopMargin={index > 0}
-                  clickHandler={() => {
-                    props.selectWorkoutCategoryHandler(category);
-                  }}
-                  justify={isOdd(index) ? 'flex-end' : 'flex-start'}
-                />
-              </Grid>
-            );
-          }
-        )}
-
-        {props.displayEditSet && <EditSet />}
-      </Grid>
-    </Grid>
+    <BaseWorkoutScreen
+      centerComponent={
+        <>
+          {props.workoutCategories.map(
+            (category: WorkoutCategoryVO, index: number) => {
+              return (
+                <Grid key={category.id} item xs={12}>
+                  <SelectionCard
+                    title={category.name}
+                    addTopMargin={index > 0}
+                    clickHandler={() => {
+                      props.selectWorkoutCategoryHandler(category);
+                    }}
+                    justify={isOdd(index) ? 'flex-end' : 'flex-start'}
+                  />
+                </Grid>
+              );
+            }
+          )}
+        </>
+      }
+    />
   );
 };
 
