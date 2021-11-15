b83388446c2374261870bbd304445d840486e523
joe-yarbrough <joe@afternightfall.com>
2021-11-15 08:16:16 -0600

    setup with base

--- src/components/top-level-components/workout-screens/3-preview-workout/PreviewWorkoutScreen.tsx
+++ src/components/top-level-components/workout-screens/3-preview-workout/PreviewWorkoutScreen.tsx
@@ -4,22 +4,16 @@ import {
   WithStyles,
   StyledComponentProps,
 } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
 import React, { Component } from 'react';
 import { Styles } from '@material-ui/styles';
 import PreviewWorkoutList from './components/PreviewWorkoutList';
+import BaseWorkoutScreen from '../shared-components/BaseWorkoutScreen';
 
 const styles: Styles<Theme, StyledComponentProps> = () => ({});
 
 class ExercisesScreen extends Component<ExercisesScreenProps> {
   render(): JSX.Element {
-    return (
-      <Grid container justify={'center'}>
-        <Grid item xs={12}>
-          <PreviewWorkoutList />
-        </Grid>
-      </Grid>
-    );
+    return <BaseWorkoutScreen centerComponent={<PreviewWorkoutList />} />;
   }
 }
 
