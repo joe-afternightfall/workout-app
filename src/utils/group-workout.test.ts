9ff7cc31926d5193b227f432968a3459af2e5f8b
joe-yarbrough <joe@afternightfall.com>
2021-11-19 22:06:18 -0600

    updating

--- src/utils/group-workout.test.ts
+++ src/utils/group-workout.test.ts
@@ -128,39 +128,39 @@ describe('group workout util', () => {
     expect(Object.keys(groupedWorkouts['2019']).length).toEqual(4);
 
     // checks for year 2021
-    expect(groupedWorkouts['2021']['November'].length).toEqual(5);
-    expect(groupedWorkouts['2021']['November'][0].date).toEqual('11/04/2021');
-    expect(groupedWorkouts['2021']['November'][1].date).toEqual('11/10/2021');
-    expect(groupedWorkouts['2021']['November'][2].date).toEqual('11/14/2021');
-    expect(groupedWorkouts['2021']['November'][3].date).toEqual('11/14/2021');
+    expect(groupedWorkouts['2021']['Nov'].length).toEqual(5);
+    expect(groupedWorkouts['2021']['Nov'][0].date).toEqual('11/04/2021');
+    expect(groupedWorkouts['2021']['Nov'][1].date).toEqual('11/10/2021');
+    expect(groupedWorkouts['2021']['Nov'][2].date).toEqual('11/14/2021');
+    expect(groupedWorkouts['2021']['Nov'][3].date).toEqual('11/14/2021');
 
-    expect(groupedWorkouts['2021']['October'].length).toEqual(3);
-    expect(groupedWorkouts['2021']['October'][0].date).toEqual('10/05/2021');
-    expect(groupedWorkouts['2021']['October'][1].date).toEqual('10/21/2021');
-    expect(groupedWorkouts['2021']['October'][2].date).toEqual('10/25/2021');
+    expect(groupedWorkouts['2021']['Oct'].length).toEqual(3);
+    expect(groupedWorkouts['2021']['Oct'][0].date).toEqual('10/05/2021');
+    expect(groupedWorkouts['2021']['Oct'][1].date).toEqual('10/21/2021');
+    expect(groupedWorkouts['2021']['Oct'][2].date).toEqual('10/25/2021');
 
-    expect(groupedWorkouts['2021']['September'].length).toEqual(1);
-    expect(groupedWorkouts['2021']['September'][0].date).toEqual('09/15/2021');
+    expect(groupedWorkouts['2021']['Sept'].length).toEqual(1);
+    expect(groupedWorkouts['2021']['Sept'][0].date).toEqual('09/15/2021');
 
     // checks for year 2020
-    expect(groupedWorkouts['2020']['June'].length).toEqual(1);
-    expect(groupedWorkouts['2020']['June'][0].date).toEqual('06/13/2020');
+    expect(groupedWorkouts['2020']['Jun'].length).toEqual(1);
+    expect(groupedWorkouts['2020']['Jun'][0].date).toEqual('06/13/2020');
 
-    expect(groupedWorkouts['2020']['August'].length).toEqual(1);
-    expect(groupedWorkouts['2020']['August'][0].date).toEqual('08/28/2020');
+    expect(groupedWorkouts['2020']['Aug'].length).toEqual(1);
+    expect(groupedWorkouts['2020']['Aug'][0].date).toEqual('08/28/2020');
 
     // checks for year 2019
-    expect(groupedWorkouts['2019']['July'].length).toEqual(1);
-    expect(groupedWorkouts['2019']['July'][0].date).toEqual('07/11/2019');
+    expect(groupedWorkouts['2019']['Jul'].length).toEqual(1);
+    expect(groupedWorkouts['2019']['Jul'][0].date).toEqual('07/11/2019');
 
-    expect(groupedWorkouts['2019']['September'].length).toEqual(1);
-    expect(groupedWorkouts['2019']['September'][0].date).toEqual('09/14/2019');
+    expect(groupedWorkouts['2019']['Sept'].length).toEqual(1);
+    expect(groupedWorkouts['2019']['Sept'][0].date).toEqual('09/14/2019');
 
-    expect(groupedWorkouts['2019']['October'].length).toEqual(1);
-    expect(groupedWorkouts['2019']['October'][0].date).toEqual('10/10/2019');
+    expect(groupedWorkouts['2019']['Oct'].length).toEqual(1);
+    expect(groupedWorkouts['2019']['Oct'][0].date).toEqual('10/10/2019');
 
-    expect(groupedWorkouts['2019']['December'].length).toEqual(2);
-    expect(groupedWorkouts['2019']['December'][0].date).toEqual('12/10/2019');
-    expect(groupedWorkouts['2019']['December'][1].date).toEqual('12/15/2019');
+    expect(groupedWorkouts['2019']['Dec'].length).toEqual(2);
+    expect(groupedWorkouts['2019']['Dec'][0].date).toEqual('12/10/2019');
+    expect(groupedWorkouts['2019']['Dec'][1].date).toEqual('12/15/2019');
   });
 });
